66d3a439e8b81b291f25571dba53c8e3e13cc2d9
Unknown <kevindang97@gmail.com>
2017-11-05 00:25:01 +1030

    Formatted existing code to match Google's Java Style Guide
    Also removed the private default constructor because I was stupid and you don't need to make it private to prevent people from calling the default constructor

--- src/com/gmail/kevindang97/tvseriesrenamer/FileRenamer.java
+++ src/com/gmail/kevindang97/tvseriesrenamer/FileRenamer.java
@@ -5,91 +5,92 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 /**
- * Abstract data structure used to keep all the data associated with a single renaming action
- * 	 in one convenient object
+ * Abstract data structure used to keep all the data associated with a single renaming action in one
+ * convenient object
  */
 public class FileRenamer {
-	
-	private Path sourceFile;
-	private String endFilename;
-	
-	/**
-	 * Private default constructor so that it can't be called
-	 */
-	@SuppressWarnings("unused")
-	private FileRenamer() {}
-	
-	/**
-	 * Creates a new FileRenamer object and sets both parameters
-	 * @param sourceFile
-	 * @param endFilename
-	 */
-	public FileRenamer(Path sourceFile, String endFilename) {
-		
-	}
-	
-	/**
-	 * Returns the Path of the source file used in this object
-	 * @return
-	 */
-	public Path getSourceFile() {
-		return sourceFile;
-	}
-	
-	/**
-	 * Set a new source file path
-	 * @param sourceFile
-	 */
-	public void setSourceFile(Path sourceFile) {
-		this.sourceFile = sourceFile;
-	}
-	
-	/**
-	 * Returns the String of the filename that the source file will be renamed to
-	 * @return
-	 */
-	public String getEndFilename() {
-		return endFilename;
-	}
-	
-	/**
-	 * Set a new end filename that the file will be renamed to 
-	 * @param endFilename
-	 */
-	public void setEndFilename(String endFilename) {
-		this.endFilename = endFilename;
-	}
-	
-	/**
-	 * Swaps the source files of two FileRenamer objects with each other
-	 * @param other
-	 */
-	public void swapSourceFiles(FileRenamer other) {
-		Path temp = this.getSourceFile();
-		this.setSourceFile(other.getSourceFile());
-		other.setSourceFile(temp);
-	}
-	
-	/**
-	 * Swaps the end filenames of two FileRenamer objects with each other
-	 * @param other
-	 */
-	public void swapEndFilenames(FileRenamer other) {
-		String temp = this.getEndFilename();
-		this.setEndFilename(other.getEndFilename());
-		other.setEndFilename(temp);
-	}
-	
-	/**
-	 * Does the renaming action, setting the new filename of the source file to endFilename
-	 */
-	public void rename() {
-		try {
-			Files.move(sourceFile, sourceFile.resolve(endFilename));
-		} catch (IOException e) {
-			System.out.println("IOException occurred in FileRenamer object's rename() method");
-			e.printStackTrace();
-		}
-	}
-	
+
+  private Path sourceFile;
+  private String endFilename;
+
+  /**
+   * Creates a new FileRenamer object and sets both parameters
+   * 
+   * @param sourceFile
+   * @param endFilename
+   */
+  public FileRenamer(Path sourceFile, String endFilename) {
+
+  }
+
+  /**
+   * Returns the Path of the source file used in this object
+   * 
+   * @return
+   */
+  public Path getSourceFile() {
+    return sourceFile;
+  }
+
+  /**
+   * Set a new source file path
+   * 
+   * @param sourceFile
+   */
+  public void setSourceFile(Path sourceFile) {
+    this.sourceFile = sourceFile;
+  }
+
+  /**
+   * Returns the String of the filename that the source file will be renamed to
+   * 
+   * @return
+   */
+  public String getEndFilename() {
+    return endFilename;
+  }
+
+  /**
+   * Set a new end filename that the file will be renamed to
+   * 
+   * @param endFilename
+   */
+  public void setEndFilename(String endFilename) {
+    this.endFilename = endFilename;
+  }
+
+  /**
+   * Swaps the source files of two FileRenamer objects with each other
+   * 
+   * @param other
+   */
+  public void swapSourceFiles(FileRenamer other) {
+    Path temp = this.getSourceFile();
+    this.setSourceFile(other.getSourceFile());
+    other.setSourceFile(temp);
+  }
+
+  /**
+   * Swaps the end filenames of two FileRenamer objects with each other
+   * 
+   * @param other
+   */
+  public void swapEndFilenames(FileRenamer other) {
+    String temp = this.getEndFilename();
+    this.setEndFilename(other.getEndFilename());
+    other.setEndFilename(temp);
+  }
+
+  /**
+   * Does the renaming action, setting the new filename of the source file to endFilename
+   */
+  public void rename() {
+    try {
+      Files.move(sourceFile, sourceFile.resolve(endFilename));
+    } catch (IOException e) {
+      System.out.println("IOException occurred in FileRenamer object's rename() method");
+      e.printStackTrace();
+    }
+  }
+
 }
--- src/com/gmail/kevindang97/tvseriesrenamer/SeriesFormatter.java
+++ src/com/gmail/kevindang97/tvseriesrenamer/SeriesFormatter.java
@@ -1,157 +1,161 @@
 package com.gmail.kevindang97.tvseriesrenamer;
 
 /**
- * Creates formatted strings intended for use as the filenames of shows in a series  
+ * Creates formatted strings intended for use as the filenames of shows in a series
  */
 public class SeriesFormatter {
-	
-	private static final String DEFAULT_FORMAT = "%t - s%se%e[ - %n]";
-	private static final int DEFAULT_PADDING = 2;
-	
-	private String originalFormat;
-	private String intermediateFormat;
-	private String optionalEpisodeNameFormat;
-	private String seriesTitle;
-	private int seasonNumber;
-	private int seasonNumberPadding;
-	private int episodeNumberPadding;
-	
-	public SeriesFormatter() {
-		this(DEFAULT_FORMAT, DEFAULT_PADDING, DEFAULT_PADDING);
-	}
-	
-	public SeriesFormatter(String format) {
-		this(format, DEFAULT_PADDING, DEFAULT_PADDING);
-	}
-	
-	public SeriesFormatter(String format, int seasonNumberPadding, int episodeNumberPadding) {
-		this.originalFormat = "";
-		this.intermediateFormat = "";
-		this.optionalEpisodeNameFormat = "";
-		this.seriesTitle = "";
-		this.seasonNumber = 0;
-		this.seasonNumberPadding = seasonNumberPadding;
-		this.episodeNumberPadding = episodeNumberPadding;
-		setFormat(format);
-	}
-	
-	/**
-	 * Assigns a new format to the Formatter, special characters:
-	 * %t - series title
-	 * %s - season number
-	 * %e - episode number
-	 * [] - optional section for episode name, as this is optional  
-	 * %n - episode name (encased inside square brackets
-	 * @param format
-	 */
-	public void setFormat(String format) {
-		int startOptionalSectionIndex = format.indexOf('[');
-		int endOptionalSectionIndex = format.indexOf(']');
-		
-		if (startOptionalSectionIndex != -1 && endOptionalSectionIndex != -1) {
-			// extract optional section of the format
-			optionalEpisodeNameFormat = format.substring(startOptionalSectionIndex + 1, endOptionalSectionIndex);
-			originalFormat = format.substring(0, startOptionalSectionIndex + 1);
-			
-			// add any extra bits after the optional section to the originalFormat if they exist
-			if (format.length() > endOptionalSectionIndex) {
-				originalFormat += format.substring(endOptionalSectionIndex);
-			}
-		} else {
-			optionalEpisodeNameFormat = "";
-			originalFormat = format;
-		}
-		
-		generateIntermediateFormat();
-	}
-	
-	/**
-	 * Generates and sets a new intermediateFormat, which is used to save processing steps
-	 * 	because in normal operation a bulk number of getFormattedString() calls will be made
-	 * Will be set to an empty string if seriesTitle is also an empty string,
-	 * 	as seriesTitle is a required attribute 
-	 */
-	private void generateIntermediateFormat() {
-		if (seriesTitle != "") {
-			intermediateFormat = originalFormat.replace("%t", seriesTitle);
-			if (seasonNumber != 0) {
-				intermediateFormat = intermediateFormat.replace("%s",
-						String.format("%0" + seasonNumberPadding + "d", seasonNumber));
-			}
-		} else {
-			intermediateFormat = "";
-		}
-	}
 
-	/**
-	 * Set series title for the formatter
-	 * @param seriesTitle
-	 */
-	public void setSeriesTitle(String seriesTitle) {
-		this.seriesTitle = seriesTitle;
-		generateIntermediateFormat();
-	}
-	
-	/**
-	 * Set season number for the formatter
-	 * @param seasonNumber
-	 */
-	public void setSeasonNumber(int seasonNumber) {
-		this.seasonNumber = seasonNumber;
-		generateIntermediateFormat();
-	}
-	
-	/**
-	 * Set width of the padding used for the season number
-	 * @param seasonNumberPadding
-	 */
-	public void setSeasonPadding(int seasonNumberPadding) {
-		this.seasonNumberPadding = seasonNumberPadding;
-		generateIntermediateFormat();
-	}
-	
-	/**
-	 * Set width of the padding used for the episode number
-	 * @param episodeNumberPadding
-	 */
-	public void setEpisodePadding(int episodeNumberPadding) {
-		this.episodeNumberPadding = episodeNumberPadding;
-	}
-	
-	/**
-	 * Returns the original format set earlier using setFormat()
-	 * @return
-	 */
-	public String getOriginalFormat() {
-		return originalFormat.replace("[]", "[" + optionalEpisodeNameFormat + "]");
-	}
-	
-	/**
-	 * Main function, returns a formatted string built using the format, series title,
-	 * 	season number and given episode number
-	 * @param episodeNumber
-	 * @return
-	 */
-	public String getFormattedString(int episodeNumber) {
-		String formatted = intermediateFormat.replace("%e",
-				String.format("%0" + episodeNumberPadding + "d", episodeNumber));
-		formatted = formatted.replace("[]", "");
-		return formatted;
-	}
-	
-	/**
-	 * Main function, returns a formatted string built using the format, series title,
-	 * 	season number, given episode number and given episode name
-	 * @param episodeNumber
-	 * @param episodeName
-	 * @return
-	 */
-	public String getFormattedString(int episodeNumber, String episodeName) {
-		String formatted = intermediateFormat.replace("%e",
-				String.format("%0" + episodeNumberPadding + "d", episodeNumber));
-		formatted = formatted.replace("[]",
-				optionalEpisodeNameFormat.replace("%n", episodeName));
-		return formatted;
-	}
-	
+  private static final String DEFAULT_FORMAT = "%t - s%se%e[ - %n]";
+  private static final int DEFAULT_PADDING = 2;
+
+  private String originalFormat;
+  private String intermediateFormat;
+  private String optionalEpisodeNameFormat;
+  private String seriesTitle;
+  private int seasonNumber;
+  private int seasonNumberPadding;
+  private int episodeNumberPadding;
+
+  public SeriesFormatter() {
+    this(DEFAULT_FORMAT, DEFAULT_PADDING, DEFAULT_PADDING);
+  }
+
+  public SeriesFormatter(String format) {
+    this(format, DEFAULT_PADDING, DEFAULT_PADDING);
+  }
+
+  public SeriesFormatter(String format, int seasonNumberPadding, int episodeNumberPadding) {
+    this.originalFormat = "";
+    this.intermediateFormat = "";
+    this.optionalEpisodeNameFormat = "";
+    this.seriesTitle = "";
+    this.seasonNumber = 0;
+    this.seasonNumberPadding = seasonNumberPadding;
+    this.episodeNumberPadding = episodeNumberPadding;
+    setFormat(format);
+  }
+
+  /**
+   * Assigns a new format to the Formatter, special characters: %t - series title %s - season number
+   * %e - episode number [] - optional section for episode name, as this is optional %n - episode
+   * name (encased inside square brackets
+   * 
+   * @param format
+   */
+  public void setFormat(String format) {
+    int startOptionalSectionIndex = format.indexOf('[');
+    int endOptionalSectionIndex = format.indexOf(']');
+
+    if (startOptionalSectionIndex != -1 && endOptionalSectionIndex != -1) {
+      // extract optional section of the format
+      optionalEpisodeNameFormat =
+          format.substring(startOptionalSectionIndex + 1, endOptionalSectionIndex);
+      originalFormat = format.substring(0, startOptionalSectionIndex + 1);
+
+      // add any extra bits after the optional section to the originalFormat if they exist
+      if (format.length() > endOptionalSectionIndex) {
+        originalFormat += format.substring(endOptionalSectionIndex);
+      }
+    } else {
+      optionalEpisodeNameFormat = "";
+      originalFormat = format;
+    }
+
+    generateIntermediateFormat();
+  }
+
+  /**
+   * Generates and sets a new intermediateFormat, which is used to save processing steps because in
+   * normal operation a bulk number of getFormattedString() calls will be made Will be set to an
+   * empty string if seriesTitle is also an empty string, as seriesTitle is a required attribute
+   */
+  private void generateIntermediateFormat() {
+    if (seriesTitle != "") {
+      intermediateFormat = originalFormat.replace("%t", seriesTitle);
+      if (seasonNumber != 0) {
+        intermediateFormat = intermediateFormat.replace("%s",
+            String.format("%0" + seasonNumberPadding + "d", seasonNumber));
+      }
+    } else {
+      intermediateFormat = "";
+    }
+  }
+
+  /**
+   * Set series title for the formatter
+   * 
+   * @param seriesTitle
+   */
+  public void setSeriesTitle(String seriesTitle) {
+    this.seriesTitle = seriesTitle;
+    generateIntermediateFormat();
+  }
+
+  /**
+   * Set season number for the formatter
+   * 
+   * @param seasonNumber
+   */
+  public void setSeasonNumber(int seasonNumber) {
+    this.seasonNumber = seasonNumber;
+    generateIntermediateFormat();
+  }
+
+  /**
+   * Set width of the padding used for the season number
+   * 
+   * @param seasonNumberPadding
+   */
+  public void setSeasonPadding(int seasonNumberPadding) {
+    this.seasonNumberPadding = seasonNumberPadding;
+    generateIntermediateFormat();
+  }
+
+  /**
+   * Set width of the padding used for the episode number
+   * 
+   * @param episodeNumberPadding
+   */
+  public void setEpisodePadding(int episodeNumberPadding) {
+    this.episodeNumberPadding = episodeNumberPadding;
+  }
+
+  /**
+   * Returns the original format set earlier using setFormat()
+   * 
+   * @return
+   */
+  public String getOriginalFormat() {
+    return originalFormat.replace("[]", "[" + optionalEpisodeNameFormat + "]");
+  }
+
+  /**
+   * Main function, returns a formatted string built using the format, series title, season number
+   * and given episode number
+   * 
+   * @param episodeNumber
+   * @return
+   */
+  public String getFormattedString(int episodeNumber) {
+    String formatted = intermediateFormat.replace("%e",
+        String.format("%0" + episodeNumberPadding + "d", episodeNumber));
+    formatted = formatted.replace("[]", "");
+    return formatted;
+  }
+
+  /**
+   * Main function, returns a formatted string built using the format, series title, season number,
+   * given episode number and given episode name
+   * 
+   * @param episodeNumber
+   * @param episodeName
+   * @return
+   */
+  public String getFormattedString(int episodeNumber, String episodeName) {
+    String formatted = intermediateFormat.replace("%e",
+        String.format("%0" + episodeNumberPadding + "d", episodeNumber));
+    formatted = formatted.replace("[]", optionalEpisodeNameFormat.replace("%n", episodeName));
+    return formatted;
+  }
+
 }
