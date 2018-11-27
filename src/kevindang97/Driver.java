d9072e3555d9ee042925cbf40a098ee201bcc711
Kevin Dang <kevin.dang@student.adelaide.edu.au>
2018-11-27 23:35:14 +1030

    Works to some degree, need to test more

--- src/kevindang97/Driver.java
+++ src/kevindang97/Driver.java
@@ -1,8 +1,23 @@
 package kevindang97;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Driver {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    Path p = Paths.get("C:\\Users\\kevin\\Documents\\test");
+
+    SeriesRenamer sr = new SeriesRenamer();
+    sr.setSeriesName("Series");
+    sr.setSeasonNumber(3);
+    sr.openFolder(p);
+
+    for (int i = 0; i < sr.getNumFiles(); i++) {
+      System.out.format("File: %s, target: %s%n", sr.getCurrentFilename(i),
+          sr.getTargetFilename(i));
+    }
+
+    sr.performRename();
   }
 }
--- src/kevindang97/SeriesRenamer.java
+++ src/kevindang97/SeriesRenamer.java
@@ -1,11 +1,15 @@
 package kevindang97;
 
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SeriesRenamer {
 
+  private Path folder;
   private List<Path> files;
   private String format;
   private String seriesName;
@@ -14,6 +18,7 @@ public class SeriesRenamer {
   private List<String> episodeName;
 
   public SeriesRenamer() {
+    folder = null;
     files = new ArrayList<Path>();
     format = "[series-name] - s[season-num]e[episode-num] - [episode-name]";
     seriesName = "";
@@ -32,32 +37,30 @@ public class SeriesRenamer {
 
   public String getCurrentFilename(int index) {
     if (indexInBounds(index)) {
-      return files.get(index).toString();
+      return files.get(index).getFileName().toString();
     } else {
       System.err.println("ERROR: getCurrentFilename(), index " + index + " out of bounds");
       return "";
     }
   }
 
-  private int getNumDigits(int num) {
-    int numDigits = 0;
-    while (num > 0) {
-      num /= 10;
-      numDigits++;
-    }
-
-    return numDigits;
-  }
-
   public String getTargetFilename(int index) {
     // TODO implement support for different formats
+    // get extension from original filename
+    String extension = "";
+    String[] split = getCurrentFilename(index).split("\\.");
+    if (split.length > 1) {
+      // if the file extension actually exists
+      extension = "." + split[split.length - 1];
+    }
+
     if (indexInBounds(index)) {
       if (getEpisodeName(index).equals("")) {
-        return String.format("%s - s%02de%0" + episodeNumDigits + "d", seriesName, seasonNumber,
-            index);
+        return String.format("%s - s%02de%0" + episodeNumDigits + "d%s", seriesName, seasonNumber,
+            index + 1, extension);
       } else {
-        return String.format("%s - s%02de%0" + episodeNumDigits + "d - %s", seriesName,
-            seasonNumber, index, getEpisodeName(index));
+        return String.format("%s - s%02de%0" + episodeNumDigits + "d - %s%s", seriesName,
+            seasonNumber, index + 1, getEpisodeName(index), extension);
       }
     } else {
       System.err.println("ERROR: getTargetFilename(), index " + index + " out of bounds");
@@ -101,8 +104,30 @@ public class SeriesRenamer {
   }
 
   public boolean openFolder(Path folder) {
-    // TODO
-    return false;
+    // clear existing files and episodeName lists
+    files.clear();
+    episodeName.clear();
+
+    // opens the folder directory and stores files in private list
+    this.folder = folder;
+    try (DirectoryStream<Path> stream = Files.newDirectoryStream(folder)) {
+      for (Path path : stream) {
+        files.add(path);
+        episodeName.add("");
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      return false;
+    }
+
+    if (getNumFiles() > 99) {
+      episodeNumDigits = getNumDigits(getNumFiles());
+    } else {
+      episodeNumDigits = 2;
+    }
+
+    return true;
+
   }
 
   public void moveFileSwap(int index1, int index2) {
@@ -124,11 +149,41 @@ public class SeriesRenamer {
       return;
     }
 
-
+    if (index1 < index2) {
+      while (index1 < index2) {
+        moveFileSwap(index1, index1 + 1);
+        index1++;
+      }
+    } else if (index1 > index2) {
+      while (index1 > index2) {
+        moveFileSwap(index1, index1 - 1);
+        index1--;
+      }
+    }
   }
 
   public boolean performRename() {
+    // TODO Can't handle case where original and target filenames overlap
+    for (int i = 0; i < getNumFiles(); i++) {
+      try {
+        Files.move(folder.resolve(getCurrentFilename(i)), folder.resolve(getTargetFilename(i)));
+      } catch (IOException e) {
+        System.err.format("Failed trying to rename %s to %s, printing stack trace:%n",
+            folder.resolve(getCurrentFilename(i)), folder.resolve(getTargetFilename(i)));
+        e.printStackTrace();
+      }
+    }
     return false;
+  }
+
+  private int getNumDigits(int num) {
+    int numDigits = 0;
+    while (num > 0) {
+      num /= 10;
+      numDigits++;
+    }
+
+    return numDigits;
   }
 
   private boolean indexInBounds(int index) {
